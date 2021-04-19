2eb3c648e84c13e636c80132cde2f540703b3c6a
Antoniya <anemtserova@gmail.com>
2021-04-19 17:13:09 +0000

    final

---
+++ domain_generator.js
@@ -0,0 +1,18 @@
+let pronoun = ['the', 'my', "your"];
+let adj = ['great', 'happy', "hidden"];
+let noun = ['brain', 'garden', 'codebook'];
+let domExt = ['.com', '.me', '.pro'];
+
+let customDomain=(arrA, arrB, arrC, arrD)=>{
+	for (let itemA of arrA) {
+  	    for (let itemB of arrB) {
+    	    for (let itemC of arrC) {
+      	        for (let itemD of arrD) {
+        	        console.log(itemA+itemB+itemC+itemD);
+        }
+      }
+    }
+  }
+}
+
+customDomain(pronoun, adj, noun, domExt);
