a1c077d637f8e9307dc6921c167adc9a08ffd7ee
Antoniya <anemtserova@gmail.com>
2021-04-19 17:18:32 +0000

    final final

--- domain_generator.js
+++ domain_generator.js
@@ -4,10 +4,6 @@ let noun = ['brain', 'garden', 'codebook'];
 let domExt = ['.com', '.me', '.pro'];
 
 let customDomain=(arrA, arrB, arrC, arrD)=>{
-<<<<<<< HEAD
-=======
-	//let cusName='';
->>>>>>> 8faafe455ea3cedd914eb9242a5aeded99c4a8eb
 	for (let itemA of arrA) {
   	    for (let itemB of arrB) {
     	    for (let itemC of arrC) {
